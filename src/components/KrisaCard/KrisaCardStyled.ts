48d799e8d57675ed41c0bfb10b44763c3015a0c9
Alex Andru <alex007d@gmail.com>
2023-12-16 16:40:29 +0100

    Add KrisaCardStyled

---
+++ src/components/KrisaCard/KrisaCardStyled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const KrisaCardStyled = styled.article`
+  display: flex;
+  flex-direction: column;
+
+  .krisa {
+    &__id {
+      font-size: 2rem;
+      color: #fff;
+      text-align: center;
+      padding: 2px;
+    }
+    &__image {
+      height: 330px;
+      width: 330px;
+    }
+  }
+`;
+
+export default KrisaCardStyled;
