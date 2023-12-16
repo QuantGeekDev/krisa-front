0631badd986923857e8578c9711e157e1dc1e666
Alex Andru <alex007d@gmail.com>
2023-12-16 15:49:40 +0100

    Add KrisaLairPage tests

---
+++ src/pages/KrisaLair/KrisaLairPage.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import customRender from "../../testUtils/customRender";
+import KrisaLairPage from "./KrisaLairPage";
+
+describe("Given a KrisaLairPage page", () => {
+  describe("When it is rendered", () => {
+    test("Then the title 'Krisa Lair' should be visible", () => {
+      const expectedTitle = "Krisa Lair";
+
+      customRender(<KrisaLairPage />);
+
+      const actualKrisaLairTitle = screen.getByRole("heading", {
+        name: expectedTitle,
+      });
+
+      expect(actualKrisaLairTitle).toBeVisible();
+    });
+  });
+});
