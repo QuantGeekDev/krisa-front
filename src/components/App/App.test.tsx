e5c14f2b2bc728b24039c6bfb032eec8167e795b
Alex Andru <alex007d@gmail.com>
2023-12-16 17:20:54 +0100

    Test App

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,9 @@
+import userEvent from "@testing-library/user-event";
+
+describe("Given an App component", () => {
+  describe("When the user is on the Homepage and clicks 'Get the miska now xD' button", () => {
+    test("Then it should redirect to the Krisa Lair and 'KRISA' should be visible", () => {
+      const user = userEvent.setup();
+    });
+  });
+});
