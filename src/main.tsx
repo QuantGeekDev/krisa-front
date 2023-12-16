d89ccbb8a1375dcbf66f578b9336f6fc4c5347d0
Alex Andru <alex007d@gmail.com>
2023-12-16 15:06:36 +0100

    Removed unused import in main

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import { ThemeProvider } from "styled-components";
 import original from "react95/dist/themes/original";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./themes/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
