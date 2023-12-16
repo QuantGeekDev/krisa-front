9438df4d485727f2b94065c96ee54d6c4ad0a5f1
Alex Andru <alex007d@gmail.com>
2023-12-16 15:49:20 +0100

    Add KrisaLairPage

---
+++ src/pages/KrisaLair/KrisaLairPage.tsx
@@ -0,0 +1,11 @@
+import KrisaLairPageStyled from "./KrisaLairPageStyled";
+
+const KrisaLairPage = (): React.ReactElement => {
+  return (
+    <KrisaLairPageStyled className="krisa-lair">
+      <h1 className="krisa-lair__title"> Krisa Lair</h1>
+    </KrisaLairPageStyled>
+  );
+};
+
+export default KrisaLairPage;
