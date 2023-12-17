8c99abe92265cd5e48f43adcf4915255c9ce0f66
Alex Andru <alex007d@gmail.com>
2023-12-17 19:19:03 +0100

    Add getKrisa calls to useEffect in KrisaLairPage

--- src/pages/KrisaLair/KrisaLairPage.tsx
+++ src/pages/KrisaLair/KrisaLairPage.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import KrisaList from "../../components/KrisaList/KrisaList";
 import KrisaLairPageStyled from "./KrisaLairPageStyled";
+import { KrisaFromDb } from "../../types";
+import useKrisaApi from "../../hooks/useKrisaApi";
 
 const KrisaLairPage = (): React.ReactElement => {
+  const [krisas, setKrisas] = useState([] as KrisaFromDb[]);
+  const { getKrisas } = useKrisaApi();
+  useEffect(() => {
+    (async () => {
+      const krisas = await getKrisas();
+      setKrisas(krisas);
+    })();
+  }, [getKrisas]);
+
   return (
     <KrisaLairPageStyled className="krisa-lair">
       <h1 className="krisa-lair__title"> Krisa Lair</h1>
-      <KrisaList />
+      <KrisaList krisas={krisas} />
     </KrisaLairPageStyled>
   );
 };
