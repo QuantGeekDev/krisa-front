c9911b00d11ccd06bca306f4fe79b65fcb0b11ed
Alex Andru <alex007d@gmail.com>
2023-12-17 19:41:28 +0100

    Refactor mocks to work with KrisaFromDb type

--- src/mocks/KrisaMocks/KrisaMocks.tsx
+++ src/mocks/KrisaMocks/KrisaMocks.tsx
@@ -1,4 +1,4 @@
-import { KrisaStructure } from "../../types";
+import { KrisaFromDb, KrisaStructure } from "../../types";
 
 export const krisa1Mock: KrisaStructure = {
   krisaNumber: 1,
@@ -30,261 +30,43 @@ export const krisa5Mock: KrisaStructure = {
     "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/ykdpsbceoculvmwl8fmu",
 };
 
-export const krisa6Mock: KrisaStructure = {
-  krisaNumber: 6,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/t7dglanzelgbouiwmtgt",
-};
-
-export const krisa7Mock: KrisaStructure = {
-  krisaNumber: 7,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/yoqucptmlitkm86qgshh",
-};
-
-export const krisa8Mock: KrisaStructure = {
-  krisaNumber: 8,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/mfxrtflycbdgkavbmbfr",
-};
-
-export const krisa9Mock: KrisaStructure = {
-  krisaNumber: 9,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/wsy0mcmos5svnuddmf3c",
-};
-
-export const krisa10Mock: KrisaStructure = {
-  krisaNumber: 10,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/vqyuuna9sni8uvhsbbak",
-};
-
-export const krisa11Mock: KrisaStructure = {
-  krisaNumber: 11,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/ecstjxwlo2q5wdp97f5l",
-};
-
-export const krisa12Mock: KrisaStructure = {
-  krisaNumber: 12,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/ltguichjzfbhhbxrfxi7",
-};
-
-export const krisa13Mock: KrisaStructure = {
-  krisaNumber: 13,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/fv2xokjcqspn4wzzpzc7",
-};
-
-export const krisa14Mock: KrisaStructure = {
-  krisaNumber: 14,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/l7ylauecibzcb6eyrjb6",
-};
-
-export const krisa15Mock: KrisaStructure = {
-  krisaNumber: 15,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/u75oelmybcedaakwuc06",
-};
-
-export const krisa16Mock: KrisaStructure = {
-  krisaNumber: 16,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/sgwdn2w1owkcbg8pcqa1",
-};
-
-export const krisa17Mock: KrisaStructure = {
-  krisaNumber: 17,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/dsnvvu7jofike0slm01s",
-};
-
-export const krisa18Mock: KrisaStructure = {
-  krisaNumber: 18,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/i6bghq69aextkxr0t2cf",
-};
-
-export const krisa19Mock: KrisaStructure = {
-  krisaNumber: 19,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/ywbk352hh1qzgvidemdu",
-};
-
-export const krisa20Mock: KrisaStructure = {
-  krisaNumber: 20,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/mxiugzfefnq53zorec6m",
-};
-
-export const krisa21Mock: KrisaStructure = {
-  krisaNumber: 21,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/aohvkiqhfhznrmycxiqs",
-};
-
-export const krisa22Mock: KrisaStructure = {
-  krisaNumber: 22,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/ykczuboms3qu6yvlrhfo",
-};
-
-export const krisa23Mock: KrisaStructure = {
-  krisaNumber: 23,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/xq3jvogz2hv8umv5pwah",
-};
-
-export const krisa24Mock: KrisaStructure = {
-  krisaNumber: 24,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/kz53bz6d1jllbwjitofy",
-};
-
-export const krisa25Mock: KrisaStructure = {
-  krisaNumber: 25,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/dtau0lcxxoqlmvcvmany",
-};
-
-export const krisa26Mock: KrisaStructure = {
-  krisaNumber: 26,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/odw0gvgoq54h8wwnqu9d",
-};
-
-export const krisa27Mock: KrisaStructure = {
-  krisaNumber: 27,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/bqlp9w10ujjxhtv5jbcw",
-};
-
-export const krisa28Mock: KrisaStructure = {
-  krisaNumber: 28,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/m1hhmqk3vudf8kw9wyus",
-};
-
-export const krisa29Mock: KrisaStructure = {
-  krisaNumber: 29,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/j89r2kdspd1ktz7uxtju",
-};
-
-export const krisa30Mock: KrisaStructure = {
-  krisaNumber: 30,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/xfnp3lsjtov2sv8sqp2n",
-};
-
-export const krisa31Mock: KrisaStructure = {
-  krisaNumber: 31,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/dxkg50f06qsxvms6nvcx",
-};
-
-export const krisa32Mock: KrisaStructure = {
-  krisaNumber: 32,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/ytakir5zdros6jiorxk3",
-};
-
-export const krisa33Mock: KrisaStructure = {
-  krisaNumber: 33,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/vmodvqg32ihzanjoxwju",
-};
-
-export const krisa34Mock: KrisaStructure = {
-  krisaNumber: 34,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/r4mxruoauktrkug9hwhf",
-};
-
-export const krisa35Mock: KrisaStructure = {
-  krisaNumber: 35,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/d0joqagwj8396nknnngt",
-};
-
-export const krisa36Mock: KrisaStructure = {
-  krisaNumber: 36,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/xfn3ohcmzl4er3mt79gb",
-};
-
-export const krisa37Mock: KrisaStructure = {
-  krisaNumber: 37,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/at32aa2racuxnuvhptvo",
-};
-
-export const krisa38Mock: KrisaStructure = {
-  krisaNumber: 38,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/neoczno4jxtqbbujjm3j",
-};
-
-export const krisa39Mock: KrisaStructure = {
-  krisaNumber: 39,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/rfffygiwhzdnxiljmian",
-};
-
-export const krisa40Mock: KrisaStructure = {
-  krisaNumber: 40,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/zckp0w8p2usfkyxeuchr",
-};
-
-export const krisa41Mock: KrisaStructure = {
-  krisaNumber: 41,
-  imageUrl:
-    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto/v1/krisas/onnixbd5sjzhjh5e0fak",
-};
-
 export const krisaMockList = [
   krisa1Mock,
   krisa2Mock,
   krisa3Mock,
   krisa4Mock,
   krisa5Mock,
-  krisa6Mock,
-  krisa7Mock,
-  krisa8Mock,
-  krisa9Mock,
-  krisa10Mock,
-  krisa11Mock,
-  krisa12Mock,
-  krisa13Mock,
-  krisa14Mock,
-  krisa15Mock,
-  krisa16Mock,
-  krisa17Mock,
-  krisa18Mock,
-  krisa19Mock,
-  krisa20Mock,
-  krisa21Mock,
-  krisa22Mock,
-  krisa23Mock,
-  krisa24Mock,
-  krisa25Mock,
-  krisa26Mock,
-  krisa27Mock,
-  krisa28Mock,
-  krisa29Mock,
-  krisa30Mock,
-  krisa31Mock,
-  krisa32Mock,
-  krisa33Mock,
-  krisa34Mock,
-  krisa35Mock,
-  krisa36Mock,
-  krisa37Mock,
-  krisa38Mock,
-  krisa39Mock,
-  krisa40Mock,
 ];
+
+export const krisa1FromDbMock: KrisaFromDb = {
+  _id: "1",
+  krisaNumber: 1,
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/oskkksbrzrsts4mqdb4p",
+};
+
+export const krisa2FromDbMock: KrisaFromDb = {
+  _id: "1",
+  krisaNumber: 2,
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/ou0w95nv7hxa0yohofan",
+};
+
+export const krisa3FromDbMock: KrisaFromDb = {
+  _id: "1",
+  krisaNumber: 3,
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/zb9evck0dh75tb9nmki4",
+};
+
+export const krisa4FromDbMock: KrisaFromDb = {
+  krisaNumber: 4,
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/lpfboo50ah4x9jlzdceg",
+};
+
+export const krisa5FromDbMock: KrisaFromDb = {
+  krisaNumber: 5,
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/f_auto,q_auto,w_330,h_330/v1/krisas/ykdpsbceoculvmwl8fmu",
+};
