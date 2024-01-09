17233c7688d16566483a7f5c4cc737216adb8f4f
Alex Andru <alex007d@gmail.com>
2024-01-09 02:18:22 +0100

    Add TarotPage component

---
+++ data/tarotRats.ts
@@ -0,0 +1,154 @@
+export interface TarotRat {
+  name: string;
+  imageUrl: string;
+}
+
+const fool: TarotRat = {
+  name: "The Fool",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762158/tarotRAts/rlw8tn6xheowvsfxlmtm.png",
+};
+
+const magician: TarotRat = {
+  name: "The Magician",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762160/tarotRAts/ch0rufk4dphzhczbjfh1.png",
+};
+
+const priestess: TarotRat = {
+  name: "The Priestess",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762159/tarotRAts/vyyas6aexkm43t3dpujw.png",
+};
+
+const empress: TarotRat = {
+  name: "The Empress",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762159/tarotRAts/fjvh7unjyyq0al9stxus.png",
+};
+
+const emperor: TarotRat = {
+  name: "The Emperor",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762160/tarotRAts/eeviw8aoq80ecuf2gykd.png",
+};
+
+const hierophant: TarotRat = {
+  name: "The Hierophant",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704760008/tarotRAts/sorzys2sqjlajwllygn3.png",
+};
+
+const lovers: TarotRat = {
+  name: "The Lovers",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762152/tarotRAts/g7aa8e8gwsltxw0qxf4v.png",
+};
+
+const chariot: TarotRat = {
+  name: "The Chariot",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762152/tarotRAts/hktzo4ogbp2jbaxvtna7.png",
+};
+
+const hermit: TarotRat = {
+  name: "The Hermit",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762152/tarotRAts/zl73kb5mqdzdt5jtvkeq.png",
+};
+
+const wheelOfFortune: TarotRat = {
+  name: "The Wheel of Fortune",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762153/tarotRAts/dn5gzfskazivqhdtrws0.png",
+};
+
+const justice: TarotRat = {
+  name: "Justice",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762154/tarotRAts/ke13q81e3ku4ww0zzsku.png",
+};
+
+const hangedKrisa: TarotRat = {
+  name: "The Hanged Krisa",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762153/tarotRAts/skterfxc8oafmaworwm3.png",
+};
+
+const death: TarotRat = {
+  name: "Death",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762155/tarotRAts/nns2ageragc9gkjcvvpj.png",
+};
+
+const temperance: TarotRat = {
+  name: "Temperance",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762154/tarotRAts/thlamrektzavd3zfodqm.png",
+};
+
+const devil: TarotRat = {
+  name: "The Devil",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762155/tarotRAts/c2fgwg2bqj7dxa2bk4e2.png",
+};
+
+const tower: TarotRat = {
+  name: "The Tower",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762156/tarotRAts/ivhurhljycuxpdrm5xok.png",
+};
+
+const star: TarotRat = {
+  name: "The Star",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762156/tarotRAts/ba5opylxxyeida3goyck.png",
+};
+
+const moon: TarotRat = {
+  name: "The Moon",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762158/tarotRAts/mtzr9uduqeptxdy7mpzl.png",
+};
+
+const sun: TarotRat = {
+  name: "The Sun",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762156/tarotRAts/yyjwdlb98cvzkdyjself.png",
+};
+
+const judgement: TarotRat = {
+  name: "Judgement",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762157/tarotRAts/e8zyrkjf7fwkrujd3ocu.png",
+};
+
+const world: TarotRat = {
+  name: "World",
+  imageUrl:
+    "https://res.cloudinary.com/derffxqir/image/upload/v1704762159/tarotRAts/nsmoghnl4bqhqdnhwmrd.png",
+};
+
+export const tarotRats: TarotRat[] = [
+  fool,
+  magician,
+  priestess,
+  chariot,
+  death,
+  devil,
+  emperor,
+  empress,
+  hangedKrisa,
+  hermit,
+  hierophant,
+  judgement,
+  justice,
+  lovers,
+  sun,
+  moon,
+  star,
+  world,
+  tower,
+  temperance,
+  wheelOfFortune,
+];
